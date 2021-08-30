81ab6e18b92171e67fe15df372fc7642a1320a35
FabianOrrego <fbn.orrego@gmail.com>
2021-08-30 15:16:54 -0500

    add change git ignore

--- server/utils/cacheResponse.js
+++ server/utils/cacheResponse.js
@@ -1,7 +1,6 @@
 
 export const cacheResponse = (res, seconds) => {
-    console.log("Env: ", process.env.ENV)
-    if(process.env.ENV !== 'development') {
-        res.set("Cache-Control", `public, max-age=${seconds}`);
-    }
+  if (process.env.ENV !== 'development') {
+    res.set("Cache-Control", `public, max-age=${seconds}`);
+  }
 }
